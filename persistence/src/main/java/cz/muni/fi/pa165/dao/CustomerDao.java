d24086250ad56b714f5f808d8c432981b945dff3
Petr Valenta <justreverse@gmail.com>
2018-10-27 01:12:16 +0200

    add javadoc templates

--- persistence/src/main/java/cz/muni/fi/pa165/dao/CustomerDao.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/CustomerDao.java
@@ -5,23 +5,62 @@ import java.util.List;
 import cz.muni.fi.pa165.entity.Customer;
 
 /**
+ * Customer entity interface
+ *
  * @author Petr Valenta
  */
 public interface CustomerDao {
     /**
+     * Persists a new customer to the database
      *
      * @param c customer to be persisted.
-     *          @throws IllegalArgumentException
+     * @throws IllegalArgumentException
+     *
      */
     public void create(Customer c);
-    
+
+    /**
+     * summary
+     *
+     * @param
+     * @return
+     * @throws
+     */
     public void remove(Customer c);
 
+    /**
+     * summary
+     *
+     * @param
+     * @return
+     * @throws
+     */
     public Customer update(Customer c);
 
+    /**
+     * summary
+     *
+     * @param
+     * @return
+     * @throws
+     */
     public Customer findById(Long id);
 
+    /**
+     * summary
+     *
+     * @param
+     * @return
+     * @throws
+     */
     public Customer findByEmail(String email);
 
+    /**
+     * summary
+     *
+     * @param
+     * @return
+     * @throws
+     */
     public List<Customer> findAll();
 }
