c1eae75fc3af6704185dbdad4d6345e7b50aaa3c
423408 <423408@mail.muni.cz>
2018-10-26 13:32:37 +0200

    Edited classes and interface for Hotel entity.

--- persistence/src/main/java/cz/muni/fi/pa165/dao/HotelDao.java
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/HotelDao.java
@@ -5,17 +5,58 @@ import java.util.List;
 
 /**
  *
+ *Interface for CRUD operations for entity hotel.
+ *
+ *@author Soňa Barteková
  *
  */
 public interface HotelDao {
 
-    public void create(Hotel p);
+    /**
+     * Persists new hotel into database.
+     * 
+     * @param h represents entity to be persisted into database.
+     */
+    public void create(Hotel h);
 
-    public void remove(Hotel p) throws IllegalArgumentException;
+    /**
+     * Remove hotel entity from database.
+     *
+     * @param h represents hotel to be removed from database.     
+     * @throws IllegalArgumentException if hotel for removing is not stored.
+     */
+    public void remove(Hotel h) throws IllegalArgumentException;
     
+    /**
+     * Update persisted entity in the database.
+     *
+     * @param h represents persisted entity to be updated.
+     * @throws IllegalArgumentException if hotel for updating is not in the database.
+     */    
+    public void update(Hotel h);
+    
+    /**
+     * Return the hotel entity with specific id.
+     *
+     * @param id represents id of the hotel entity.
+     * @return the hotel entity associated with the given id.
+     */
     public Hotel findById(Long id);
 
+    /**
+     * Return all hotels in the database.
+     *
+     * @return all hotel entities from the database.
+     */
     public List<Hotel> findAll();
 
+    /**
+     * Return hotel with given name.
+     *
+     * @param name represents name of searched hotel.
+     * @return hotel from database with given name.
+     * @throws IllegalArgumentException if name is null.
+     * @throws NoResultException if there is no hotel to be returned.
+     */
     public Hotel findByName(String name);
 }
---
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/HotelDaoImpl.java
@@ -0,0 +1,59 @@
+package cz.muni.fi.pa165.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+import cz.muni.fi.pa165.entity.Hotel;
+
+/**
+ * 
+ * Class accessing persistence context for CRUD operations for hotel class.
+ * It implements HotelDao interface.
+ * @author Soňa Barteková
+ *
+ */
+public class HotelDaoImpl implements HotelDao {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	public void create(Hotel h) {
+		em.persist(h);		
+	}
+
+	public void remove(Hotel h) throws IllegalArgumentException{
+		em.remove(h);
+	}
+
+	public void update(Hotel h) {
+        if (findById(h.getId()) == null) {
+            throw new IllegalArgumentException("Unknown hotel to update!");
+        }
+        
+		em.merge(h);
+	}
+
+	public Hotel findById(Long id) {
+		return em.find(Hotel.class, id);
+	}
+
+	public List<Hotel> findAll() {
+		return em.createQuery("select h from Hotel h", Hotel.class).getResultList();
+	}
+
+	public Hotel findByName(String name) {
+        if(name == null) {
+            throw new IllegalArgumentException("You try to find hotel by NULL name!"); 
+        }
+		try {
+			return em.createQuery("select h from Hotel h where name = :name", Hotel.class)
+					.setParameter(":name", name).getSingleResult();
+		}
+		catch(NoResultException nre) {
+			return null;
+		}
+	}
+}
