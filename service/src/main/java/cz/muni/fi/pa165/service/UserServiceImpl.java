0b1669efd76266c96280c8dadbcbe8aa92d96bd3
Miloslav Žežulka <zezulka95@gmail.com>
2018-11-11 18:46:10 +0100

    Dummy implementation was not added in the previous commit.
    Add it now.

--- service/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
@@ -1,6 +1,39 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.entity.User;
+import java.util.List;
+
 
 public class UserServiceImpl implements UserService {
+
+    @Override
+    public boolean register(User user, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<User> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean authenticate(User user, String hashedPassword) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isAdmin(User candidate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
