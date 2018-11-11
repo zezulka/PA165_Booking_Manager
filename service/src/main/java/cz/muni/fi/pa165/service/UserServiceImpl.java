8f250aaf58c43323c912308ff2d7e182798cb7db
Miloslav Žežulka <zezulka95@gmail.com>
2018-11-11 18:59:58 +0100

    Implement the UserService without the password bits.

--- service/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/service/UserServiceImpl.java
@@ -1,39 +1,74 @@
 package cz.muni.fi.pa165.service;
 
+import cz.muni.fi.pa165.dao.UserDao;
 import cz.muni.fi.pa165.entity.User;
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserDao userDao;
+    
     @Override
     public boolean register(User user, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (user == null) {
+            throw new IllegalArgumentException("User is null.");
+        }
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password must be nonempty.");
+        }
+        if (user.getId() == null) {
+            throw new IllegalArgumentException("User does not have its id set.");
+        }
+        // TODO this is wrong, of course. We cannot store passwords in plain text!
+        user.setPasswordHash(password);
+        userDao.create(user);
+        throw new UnsupportedOperationException("Password hashing is not implemented yet.");
     }
 
     @Override
     public List<User> getAll() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return userDao.findAll();
     }
 
     @Override
     public boolean authenticate(User user, String hashedPassword) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(user == null) {
+            throw new IllegalArgumentException("User is null");
+        }
+        if(hashedPassword == null || hashedPassword.isEmpty()) {
+            throw new IllegalArgumentException("Hashed password must be nonempty.");
+        }
+        if(user.getId() == null) {
+            throw new IllegalArgumentException("User does not have its id set.");
+        }
+        // TODO validate the password here
+        throw new UnsupportedOperationException("Password validation is not implemented yet.");
     }
 
     @Override
     public boolean isAdmin(User candidate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(candidate == null) {
+            throw new IllegalArgumentException("User is null");
+        }
+        return findById(candidate.getId()).isAdmin();
     }
 
     @Override
     public User findById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(id == null) {
+            throw new IllegalArgumentException("Id cannot be null.");   
+        }
+        return userDao.findById(id);
     }
 
     @Override
     public User findByEmail(String email) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(email == null) {
+            throw new IllegalArgumentException("Email cannot be null.");
+        }
+        return userDao.findByEmail(email);
     }
 
 }
