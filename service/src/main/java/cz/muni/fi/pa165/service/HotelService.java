516f9938e4821fc697461d5f4ecda742457413dc
Petr Valenta <justreverse@gmail.com>
2018-11-19 01:25:42 +0100

    add contract

--- service/src/main/java/cz/muni/fi/pa165/service/HotelService.java
+++ service/src/main/java/cz/muni/fi/pa165/service/HotelService.java
@@ -12,11 +12,68 @@ import cz.muni.fi.pa165.entity.Room;
  */
 @Service
 public interface HotelService {
+
+    /**
+     * Finds a hotel by its id.
+     *
+     * @param id non-null id
+     * @throws IllegalArgumentException id is null
+     * @throws BookingManagerDataAccessException error during service
+     * @return {@link Hotel} with the given id or null if such hotel does not exist
+     */
     Hotel findById(Long id);
+
+    /**
+     * Finds a hotel by its name.
+     *
+     * @param name non empty String
+     * @throws IllegalArgumentException name is null or empty
+     * @throws BookingManagerDataAccessException error during service
+     * @return {@link Hotel} instance with the given name or null if such hotel does not exist
+     */
+    Hotel findByName(String name);
+
+    /**
+     * Returns a list of all hotels.
+     *
+     * @throws BookingManagerDataAccessException error during service
+     * @return {@link List} of all {@link Hotel} instances.
+     */
     List<Hotel> findAll();
-    Hotel createHotel(Hotel h);
-    void addRoom(Hotel h, Room r);
-    void removeRoom(Hotel h, Room r);
-    void changeAddress(Hotel h, String a);
-    void deleteHotel(Hotel h);
+
+    /**
+     * Creates a new {@link Hotel}.
+     *
+     * @param hotel to be created
+     * @throws IllegalArgumentException hotel is null
+     * @throws BookingManagerDataAccessException error during service
+     */
+    void create(Hotel hotel);
+
+//    /**
+//     * Adds a {@link Room}.
+//     *
+//     * @param h target {@link Hotel}
+//     * @param r {@link Room} to be added
+//     * @throws IllegalArgumentException h/r is null or h/r has null id
+//     */
+//    void addRoom(Hotel hotel, Room room);
+//
+//    /**
+//     * Removes a {@link Room}.
+//     *
+//     * @param h target {@link Hotel}
+//     * @param r {@link Room} to be removed
+//     * @throws IllegalArgumentException h/r is null or h/r has null id
+//     */
+//    void removeRoom(Hotel hotel, Room room);
+
+    /**
+     * Deletes a {@link Hotel}.
+     *
+     * @param hotel {@link Hotel} to be removed
+     * @throws IllegalArgumentException hotel is null
+     * @throws BookingManagerDataAccessException error during service
+     */
+    void delete(Hotel hotel);
 }
