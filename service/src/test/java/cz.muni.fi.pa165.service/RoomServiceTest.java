6ebbbbd74da784ce9840762148b050cab2d7259b
Petr Valenta <justreverse@gmail.com>
2018-11-21 12:54:08 +0100

    implement RoomServiceTests

--- service/src/test/java/cz.muni.fi.pa165.service/RoomServiceTest.java
+++ service/src/test/java/cz.muni.fi.pa165.service/RoomServiceTest.java
@@ -1,27 +1,87 @@
 package cz.muni.fi.pa165.service;
 
-import static org.junit.Assert.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-public class RoomServiceTest {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import org.testng.annotations.BeforeMethod;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.muni.fi.pa165.entity.Hotel;
+import cz.muni.fi.pa165.entity.Room;
+import cz.muni.fi.pa165.enums.RoomType;
+import cz.muni.fi.pa165.service.config.ServiceConfiguration;
+import cz.muni.fi.pa165.dao.HotelDao;
+import cz.muni.fi.pa165.dao.RoomDao;
+
+@ContextConfiguration(classes=ServiceConfiguration.class)
+public class RoomServiceTest extends AbstractTransactionalTestNGSpringContextTests {
     @Mock
+    private RoomDao roomDao;
 
-    @Before
-    public void setUp() throws Exception {
+    @Mock
+    private HotelDao hotelDao;
+
+    @Autowired
+    @InjectMocks
+    private RoomService roomService;
+
+    @BeforeClass
+    private void SetUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    private Room r1;
+    private Hotel h1;
+
+    @BeforeMethod
+    public void init(){
+        r1 = new Room();
+        h1 = new Hotel();
+
+        r1.setDescription("Single room, beautiful view.");
+        r1.setHotel(h1);
+        r1.setImage(new byte[0]);
+        r1.setNumber(101);
+        r1.setRecommendedPrice(new BigDecimal("2000.0"));
+        r1.setType(RoomType.SINGLE_ROOM);
+
+        h1.setAddress("In The Middle Of Nowhere");
+        h1.setName("Noname");
+        List<Room> l1 = new ArrayList<>();
+        l1.add(r1);
+        h1.setRooms(l1);
     }
 
     @Test
-    public void createRoom() {
+    public void createRoomHappyTest() {
+        roomService.createRoom(r1);
+        verify(roomDao).create(r1);
     }
 
     @Test
-    public void deleteRoom() {
+    public void deleteRoomHappyTest() {
+        roomService.deleteRoom(r1);
+        verify(roomDao).remove(r1);
     }
 
     @Test
-    public void updateRoom() {
+    public void updateRoomHappyTest() {
+        roomService.updateRoom(r1);
+        verify(roomDao).update(r1);
     }
 
     @Test
@@ -30,6 +90,10 @@ public class RoomServiceTest {
 
     @Test
     public void findById() {
+        when(roomDao.findById(1L)).thenReturn(r1);
+        Room tmp = roomService.findById(1L);
+        verify(roomDao).findById(1L);
+        Assert.assertEquals(tmp, r1);
     }
 
     @Test
