7e2e44061073395b435f1b273c82ec8037b44cbd
Petr Valenta <justreverse@gmail.com>
2018-11-21 12:06:49 +0100

    create test file, add dependencies

---
+++ service/src/test/java/cz.muni.fi.pa165.service/RoomServiceTest.java
@@ -0,0 +1,42 @@
+package cz.muni.fi.pa165.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RoomServiceTest {
+    @Mock
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createRoom() {
+    }
+
+    @Test
+    public void deleteRoom() {
+    }
+
+    @Test
+    public void updateRoom() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void findByHotel() {
+    }
+
+    @Test
+    public void findByNumber() {
+    }
+}
