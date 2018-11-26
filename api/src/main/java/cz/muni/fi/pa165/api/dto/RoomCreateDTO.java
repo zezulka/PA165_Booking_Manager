e4c1cb5aa95c21817a6bf49b68fd748bc343c7b6
Martin Páleník <xpalenik@fi.muni.cz>
2018-11-26 15:54:41 +0100

    RoomCreateDTO: add implementation

--- api/src/main/java/cz/muni/fi/pa165/api/dto/RoomCreateDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/api/dto/RoomCreateDTO.java
@@ -1,9 +1,27 @@
+
+// @author Martin Palenik
+
 package cz.muni.fi.pa165.api.dto;
 
-/**
- *
- * 
- */
-public class RoomCreateDTO {
+import cz.muni.fi.pa165.entity.Hotel;
 
+public class RoomCreateDTO {
+    private Hotel hotel;
+    private Integer number;
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 }
