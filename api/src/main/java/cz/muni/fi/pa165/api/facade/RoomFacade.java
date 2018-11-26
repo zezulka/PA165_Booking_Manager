2a1779d86b6318e2df231b4a027e9760f2cb061f
Petr Valenta <justreverse@gmail.com>
2018-11-26 14:12:56 +0100

    add javadoc

--- api/src/main/java/cz/muni/fi/pa165/api/facade/RoomFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/api/facade/RoomFacade.java
@@ -5,17 +5,50 @@ import cz.muni.fi.pa165.api.dto.RoomCreateDTO;
 import cz.muni.fi.pa165.api.dto.RoomDTO;
 import java.util.List;
 
+/**
+ * @author Petr Valenta
+ */
 public interface RoomFacade {
 
+    /**
+     * Creates a room.
+     * @param room to be created
+     * @return true if created
+     */
     boolean createRoom(RoomCreateDTO room);
 
+    /**
+     * Deletes a room.
+     * @param id of the room to be deleted
+     * @return true if deleted
+     */
     boolean deleteRoom(Long id);
 
+    /**
+     * Returns a list of all rooms.
+     * @return List of all rooms
+     */
     List<RoomDTO> findAll();
 
+    /**
+     * Find a room by id.
+     * @param id to search with
+     * @return RoomDTO with the given id or null
+     */
     RoomDTO findById(Long id);
 
+    /**
+     * Finds all rooms in a given hotel.
+     * @param hotel
+     * @return List of all rooms in the given hotel
+     */
     List<RoomDTO> findByHotel(HotelDTO hotel);
 
+    /**
+     * Finds a room in a given hotel with specified number.
+     * @param hotel
+     * @param number
+     * @return found room or null
+     */
     RoomDTO findByNumber(HotelDTO hotel, Integer number);
 }
