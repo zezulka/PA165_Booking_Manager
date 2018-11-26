45295130e9a6cace4fc562647e89a224fd34fd6d
Miloslav Žežulka <zezulka95@gmail.com>
2018-11-26 16:58:27 +0100

    Implement UserFacade.

--- api/src/main/java/cz/muni/fi/pa165/api/dto/UserAuthenticateDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/api/dto/UserAuthenticateDTO.java
@@ -2,27 +2,28 @@ package cz.muni.fi.pa165.api.dto;
 
 /**
  *
- *@author Soňa Barteková
- * 
+ * @author Soňa Barteková
+ *
  */
 public class UserAuthenticateDTO {
-	
-	private Long usedId;
-	private String password;
-	
-	public Long getUsedId() {
-		return usedId;
-	}
-	public void setUsedId(Long usedId) {
-		this.usedId = usedId;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
+
+    private Long userId;
+    private String password;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long usedId) {
+        this.userId = usedId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
---
+++ service/src/main/java/cz/muni/fi/pa165/service/facade/UserFacadeImpl.java
@@ -0,0 +1,65 @@
+package cz.muni.fi.pa165.service.facade;
+
+import cz.muni.fi.pa165.api.dto.UserAuthenticateDTO;
+import cz.muni.fi.pa165.api.dto.UserDTO;
+import cz.muni.fi.pa165.api.facade.UserFacade;
+import cz.muni.fi.pa165.service.UserService;
+import cz.muni.fi.pa165.service.auxiliary.BeanMappingService;
+import cz.muni.fi.pa165.entity.User;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserFacadeImpl implements UserFacade {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private BeanMappingService beanMappingService;
+
+    @Override
+    public void register(UserDTO user, String password) {
+        if (user == null) {
+            throw new IllegalArgumentException("User must not be null.");
+        }
+        User userEntity = beanMappingService.mapTo(user, User.class);
+        userService.register(userEntity, password);
+        user.setId(userEntity.getId());
+    }
+
+    @Override
+    public List<UserDTO> getAll() {
+        return beanMappingService.mapTo(userService.getAll(), UserDTO.class);
+    }
+
+    @Override
+    public boolean authenticate(UserAuthenticateDTO auth) {
+        if (auth == null) {
+            throw new IllegalArgumentException("Authentication details cannot be null.");
+        }
+        return userService.authenticate(
+                userService.findById(auth.getUserId()), auth.getPassword());
+    }
+
+    @Override
+    public boolean isAdmin(UserDTO candidate) {
+        return userService.isAdmin(beanMappingService.mapTo(candidate, User.class));
+    }
+
+    @Override
+    public UserDTO findById(Long id) {
+        User user = userService.findById(id);
+        return (user == null) ? null : beanMappingService.mapTo(user, UserDTO.class);
+    }
+
+    @Override
+    public UserDTO findByEmail(String email) {
+        User user = userService.findByEmail(email);
+        return (user == null) ? null : beanMappingService.mapTo(user, UserDTO.class);
+    }
+
+}
